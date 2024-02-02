ef599f9cf99cfc9c4899522216338037ba777526
Nabi Mondal <113233991+Nabiweb@users.noreply.github.com>
2024-02-02 09:45:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ let search = document.querySelector("#search");
 let qrimg = document.querySelector("#qrimg");
 let loader = document.querySelector("#loading");
 
+function voice() {
+    let recog = new webkitSpeechRecognition(); 
+    recog.lang = "en-GB";
+    
+    recog.onresult = function (event) {
+        console.log(event);
+        document.getElementById("textInput").value = event.results[0][0].transcript;
+    };
+    
+    recog.start();
+}
 search.addEventListener("click", () => {
     loader.style.display = "block";
 
-    // Corrected the variable name to "input" and fixed the string interpolation
     let input = document.querySelector("#textInput").value;
-    let api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`;
+    let api = `https://quickchart.io/qr?text=${input}&dark=0d7339&ecLevel=H&margin=1&size=150`;
 
-    // Added an error handling for the image load event
     qrimg.onload = function () {
         loader.style.display = "none";
     };
 
-    // Added an error handling for the image load event
     qrimg.onerror = function () {
         alert("Failed to load the QR code image.");
         loader.style.display = "none";
